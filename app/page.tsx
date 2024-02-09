88160c7d6662b8be3330db48e5a64b2a01eac4b1
Oliver Cook <olliewatford@gmail.com>
2024-02-09 13:08:05 +0000

    Just need to make responsive

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import PrimaryColourProvider from "./components/PrimaryColourContext";
 export default function Home() {
   return (
     <main className="flex flex-col items-center w-11/12 relative left-1/2 -translate-x-1/2 min-h-screen">
-      <h1 className="text-6xl mt-4 font-bold">One Hue Palette Generator</h1>
+      <h1 className="text-6xl mt-4 font-bold">Single Hue Palette Generator</h1>
       <p className="mt-1">
         Inspired by the book <a href="https://www.refactoringui.com/" target="_blank" className="underline">Refactoring UI</a> by <a href="https://adamwathan.me/" target="_blank" className="underline">Adam Wathan</a> and <a href="https://www.steveschoger.com/" target="_blank" className="underline">Steve Schoger</a>.
       </p>
