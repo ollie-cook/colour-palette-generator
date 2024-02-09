9c82072dc378f3969a3a026e08e1dc259302b234
Oliver Cook <olliewatford@gmail.com>
2024-02-09 13:24:33 +0000

    implemented analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Roboto_Slab } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={robotoMono.className}>{children}</body>
+      <body className={robotoMono.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
